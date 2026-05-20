719e456847193f54f904c724941c1a835fd2ef30
Ngozi Lilian Okafor <okaforngozi1704@gmail.com>
2026-05-20 17:06:00 +0700

    Players selection

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ import json
 import redis
 import pika    #translator to talk to rabbitmq
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 from flask_cors import CORS   #cross origin resource sharing,
 
 
@@ -23,11 +23,17 @@ channel = connection.channel()
 channel.queue_declare(queue='game_moves')  
 #queue name: game_moves, where the game moves are sent from the frontend to the backend
 
+
+
 @app.route('/')
 def index():     #healthcheck endpoint to check if the backend is running
-    return "Nosy Neighbosr Backend is running"
+    return "Nosy Neighbor Backend is running"
 
 DEFAULT_GAME_STATE ={
+    "status": "waiting_for_players", #game starts in a waiting state until both players have joined
+    "p1_claimed": False,
+    "p2_claimed": False,
+    
     "p1": {"x":5, "y":10, "hp": 6}, #player 1 starts at the bottom middle of the grid, with 6 hp
     
     "p2": {"x":5, "y":0, "hp": 6},  #player 2 starts at the top middle of the grid, with 6 hp
@@ -44,10 +50,29 @@ def handle_join(data=None):
     if not state:
         r.set('game_state', json.dumps(DEFAULT_GAME_STATE))
         state = r.get('game_state')
-        
-        #game state is sent to frontend.
-        socketio.emit('game_update', json.loads(state))
 
+    #game state is sent to frontend.
+    emit('game_update', json.loads(state), broadcast=True)  #broadcast=True, means that the game state will be sent to all connected clients, not just the one that triggered the event
+        
+    
+@socketio.on('claim_player')
+def handle_claim(player_id):
+    state = json.loads(r.get('game_state'))
+    
+    #lock player selection so that only one player can claim p1 and the other can claim p2
+    if player_id == "p1":
+        state["p1_claimed"] = True
+        
+    elif player_id == "p2":
+        state["p2_claimed"] = True
+        
+    #if both players are choosen, game starts 
+    if state["p1_claimed"] and state ["p2_claimed"]:
+        state["status"] = "playing"
+        
+        #save to redis
+    r.set('game_state', json.dumps(state))
+    emit('game_update', state, broadcast=True)    
 
 #to start the server
 if __name__ == '__main__':
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,6 @@ const socket = io('http://localhost:5000');
 function App() {
 //gameState starts empty until Python sends the data
   const [gameState, setGameState] = useState(null);  
-
   const [myPlayerId, setMyPlayerId] = useState(null); //to track if this device is P1 or P2
 
   //creating grid for board
@@ -17,46 +16,89 @@ function App() {
 
 
   useEffect(() => {
-   socket.on('connect', () => {
+    const handleConnect = () => {
     //react asks backend for the board
     socket.emit('join_game');
-   });
+   };
+
+   const handleGameUpdate = (newState) => {
+    setGameState(newState);
+   };
+
+   socket.on('connect', handleConnect);
 
    //backend sends the board to react
-   socket.on('game_update', (newState) => {
-    setGameState(newState);
-   });
+   socket.on('game_update', handleGameUpdate);
+
+   if (socket.connected) {
+    handleConnect();
+   }
 
    ////cleanup function to to remove listeners when you close the tab
    return () => {
-    socket.off('connect');
-    socket.off('game_update');
+    socket.off('connect', handleConnect);
+    socket.off('game_update', handleGameUpdate);
     };
   }, []); //empty brackets mean "only run this setup ONE time when the page loads"
 
-   
+
+  //when a player is selected, ID gets locked and sent to backend to claim it.
+  const handleChoosePlayer = (playerId) => {
+    setMyPlayerId(playerId);
+    socket.emit('claim_player', playerId);
+  }; 
+  if (!gameState)
+    return <div className="lobby-container">
+      <h2>Loading game...</h2></div>;
+
+
+
 
 // ===============================THE LOBBY SCREEN - CHOOSE YOUR PLAYER=================================================
 if (!myPlayerId) {
   return (
     <div className="lobby-container">
       <h1>Welcome to Nosy Neighbor!</h1>
-      <h2>Please choose your player:</h2>
+      <h2>Choose your player:</h2>
     <div style={{ display: 'flex', gap: '20px', margin: '20px' }}>
 
+{/*the buttons get disabled when its already taken */}
       <button
-       onClick={() => setMyPlayerId('p1')}
-       style={{ backgroundColor: '#3498db', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}
-       >Player 1 (Blue)
+       onClick={() => handleChoosePlayer('p1')}
+       disabled={gameState.p1_claimed}
+       style={{ backgroundColor: '#3498db', color: 'white', padding: '10px 20px', 
+        border: 'none', borderRadius: '5px', cursor: 'pointer' }}
+       >{gameState.p1_claimed ? 'P1 Taken' : 'Player 1 (Blue)'}
        </button>
 
       <button 
-      onClick={() => setMyPlayerId('p2')}
+      onClick={() => handleChoosePlayer('p2')}
+      disabled={gameState.p2_claimed}
       style={{ backgroundColor: '#e74c3c', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}
-      >Player 2 (Red)
+      >{gameState.p2_claimed ? 'P2 Taken' : 'Player 2 (Red)'}
       </button>
+
     </div>
     </div>
+  );
+}
+
+//=========================WAITING ROOM - WAIT FOR OTHER PLAYER TO JOIN===========================================
+if (gameState.status === 'waiting_for_players') {
+  return(
+    <div className="waiting-room">
+      <h1 style={{ fontSize: '3rem', marginBottom: '20px', textAlign: 'center'}}>Nosy Neighbor</h1>
+      <div style={{
+        padding: '20px',
+        backgroundColor: '#f0f0f0',
+        borderRadius: '10px',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        maxWidth: '400px',
+        border: `4px solid ${myPlayerId === 'p1' ? '#3498db' : '#e74c3c'}`}}> 
+        <h2>You are {myPlayerId}</h2>
+      <h3 style={{marginTop: '20px', animation: 'pulse 1.5s infinite'}}>Waiting for other player to join...</h3>
+  </div>
+  </div>
   );
 }
 
