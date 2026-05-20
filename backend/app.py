b04c74022f4e3f8ef8df525792e5b75e9182b7a0
Ngozi Lilian Okafor <okaforngozi1704@gmail.com>
2026-05-20 14:27:43 +0700

    connecting ( frontend and backend) -> Redis & Rabbitmq

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,54 @@
+import json
+import redis
+import pika    #translator to talk to rabbitmq
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS   #cross origin resource sharing,
-import redis
-import pika    #translator to talk to rabbitmq
 
+
+#initilaize the flask app and socketio
 app = Flask(__name__)
 CORS(app)     #it allows frontend to connect with backend even if they are on different domains or ports
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*") #allows for realtime websocket communication between frontend and backend
 
-#to test redis conenction
-r = redis.Redis(host='localhost', port=6379,db=0)
+#Conenct to redis
+r = redis.Redis(host='localhost', port=6379,db=0, decode_responses=True)
+#decode_reponses=True, allows us to get normal text instead of byteswhen we get the data from redis
+
+#connect to Rabbitmq
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+# queue -> malbox
+channel.queue_declare(queue='game_moves')  
+#queue name: game_moves, where the game moves are sent from the frontend to the backend
 
 @app.route('/')
-
 def index():     #healthcheck endpoint to check if the backend is running
     return "Nosy Neighbosr Backend is running"
 
+DEFAULT_GAME_STATE ={
+    "p1": {"x":5, "y":10, "hp": 6}, #player 1 starts at the bottom middle of the grid, with 6 hp
+    
+    "p2": {"x":5, "y":0, "hp": 6},  #player 2 starts at the top middle of the grid, with 6 hp
+}
+
+@socketio.on('join_game')
+def handle_join(data=None):
+    print("A player has joined the game.")
+    
+    #to check id a game is saved in Redis
+    state = r.get('game_state')
+    
+    #if there is no game, creates a new one using the default game state
+    if not state:
+        r.set('game_state', json.dumps(DEFAULT_GAME_STATE))
+        state = r.get('game_state')
+        
+        #game state is sent to frontend.
+        socketio.emit('game_update', json.loads(state))
+
+
+#to start the server
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5000)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,34 +6,109 @@ import './App.css';
 const socket = io('http://localhost:5000');
 
 function App() {
-  const [isConencted, setISConnected] = useState(false);
+//gameState starts empty until Python sends the data
+  const [gameState, setGameState] = useState(null);  
+
+  const [myPlayerId, setMyPlayerId] = useState(null); //to track if this device is P1 or P2
+
+  //creating grid for board
+  const gridSize =11;
+  const totalcells = gridSize * gridSize;
+
+
   useEffect(() => {
-    //listen to connection event from Flask backend
-    socket.on('connect', () => {
-      setISConnected(true);
-      console.log('Nosy Neighbot is connected to Backend!');
-    });
+   socket.on('connect', () => {
+    //react asks backend for the board
+    socket.emit('join_game');
+   });
 
-    //listen to disconnection event from Flask backend
-    socket.on('disconnect', () => {
-      setIsConencted(false);
-      console.log('Nosy Neighbor is disconnected from backend')
-    });
+   //backend sends the board to react
+   socket.on('game_update', (newState) => {
+    setGameState(newState);
+   });
 
-    //ensure conenction is closed after closing tab
-    return () => {
-      socket.off('connect');
-      socket.off('disconnect');
-    }
+   ////cleanup function to to remove listeners when you close the tab
+   return () => {
+    socket.off('connect');
+    socket.off('game_update');
+    };
+  }, []); //empty brackets mean "only run this setup ONE time when the page loads"
 
-  }, []);
+   
 
+// ===============================THE LOBBY SCREEN - CHOOSE YOUR PLAYER=================================================
+if (!myPlayerId) {
   return (
-    <div className="App">
-      <h1>Nosy Neighbor</h1>
-      <p>backend status: {isConencted ? '🟢 Connected' : '🔴 Disconnected'}</p>
+    <div className="lobby-container">
+      <h1>Welcome to Nosy Neighbor!</h1>
+      <h2>Please choose your player:</h2>
+    <div style={{ display: 'flex', gap: '20px', margin: '20px' }}>
+
+      <button
+       onClick={() => setMyPlayerId('p1')}
+       style={{ backgroundColor: '#3498db', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}
+       >Player 1 (Blue)
+       </button>
+
+      <button 
+      onClick={() => setMyPlayerId('p2')}
+      style={{ backgroundColor: '#e74c3c', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}
+      >Player 2 (Red)
+      </button>
     </div>
-  )
+    </div>
+  );
 }
 
+
+
+// ===============================THE GAME BOARD SCREEN===============================================
+
+ const cells =[];
+ // this will loop 121 times to create the grid cells.
+  for (let i = 0; i < totalcells; i++) {
+    //to calculte the x and y coordinates of each cell based on its index
+    const x = i % gridSize;
+    const y = Math.floor(i / gridSize);
+
+    let cellClass = "grid-cell"; //default class for all cells
+    let cellText = ""; //default text for all cells
+  
+
+  //check if backend has given the board data.
+  if (gameState) {
+    if(gameState.p1.x === x && gameState.p1.y === y) {
+      cellClass = "grid-cell cell-p1"; 
+      cellText = "P1";
+    }
+
+    else if(gameState.p2.x === x && gameState.p2.y === y) {
+      cellClass = "grid-cell cell-p2"; 
+      cellText = "P2";
+    }
+  }
+
+    cells.push(
+      <div key={i} className={cellClass}>
+        {cellText}
+      </div>  
+    );
+}
+
+  return (
+    <div className="game-container">
+      <h1 style={{ marginBottom: '20px' }}>Nosy Neighbor</h1>
+
+      <h2 style={{ marginBottom: '10px'}}>You are: {myPlayerId === 'p1' ? 'Player 1 (Blue)' : 'Player 2 (Red)'}</h2>
+
+      {/* Game Board */}
+      <div className="board">
+        {cells}
+      </div>
+
+      </div>
+    
+  );
+
+}
 export default App;
